23f225a08383813d776d9af63d3950c5a6d8859d
John Dobie <john@dobie.co.uk>
2025-02-17 15:43:15 +0000

    add spring boot code

---
+++ src/main/java/com/example/github/action/SpringbootGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/github/action/SpringbootGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
